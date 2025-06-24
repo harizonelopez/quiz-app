1de235a9ff27a7ae2210efd7c2bc63d56c3ba071
Harizone lopez <harizonelopez23@gmail.com>
2025-06-24 11:22:27 +0300

    commits

--- app.py
+++ app.py
@@ -82,7 +82,6 @@ class Quiz:
     def is_finished(self):
         return self.current_question_index == len(self.questions)
 
-
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
     if 'username' not in session:
